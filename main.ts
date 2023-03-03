9befc52e6e6e71d44fe3e34434eac3b49e4bf691
andrea pagliari <andrea.pagliari12@gmail.com>
2023-03-03 22:59:48 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showNumber(conteggioInfrarosso)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    conteggioInfrarosso = 0
 })
 let infrarosso = 0
 let prossimita = 0
 let valore = 0
 let conteggioInfrarosso = 0
+conteggioInfrarosso = 0
+let statoPrecedenteInfrarosso = 1
+let statoInfrarosso = 1
 serial.redirectToUSB()
 basic.forever(function () {
     valore = pins.digitalReadPin(DigitalPin.P0)
@@ -13,9 +21,12 @@ basic.forever(function () {
     infrarosso = pins.digitalReadPin(DigitalPin.P2)
     serial.writeValue("x", valore)
     serial.writeValue("y", prossimita)
-    serial.writeValue("z", infrarosso)
-    basic.showNumber(infrarosso)
-    if (infrarosso == 0) {
+    serial.writeValue("z", statoInfrarosso)
+    if (statoInfrarosso == 0 && statoPrecedenteInfrarosso == 1) {
         conteggioInfrarosso += 1
+        statoPrecedenteInfrarosso = 0
+    }
+    if (statoInfrarosso == 1 && statoPrecedenteInfrarosso == 0) {
+        statoPrecedenteInfrarosso = 1
     }
 })
